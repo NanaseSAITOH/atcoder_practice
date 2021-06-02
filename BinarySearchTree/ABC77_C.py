edec659578367ed1456b5a8a55858b67a39b1fd6
nanasesaitoh <nanasesaitoh@gmail.com>
2021-06-02 22:17:40 +0900

    分からん

--- BinarySearchTree/ABC77_C.py
+++ BinarySearchTree/ABC77_C.py
@@ -1,6 +1,23 @@
 from bisect import bisect_right
 
 
+def bonary_search(B, a):
+    left = -1
+    right = N
+    while(left<right):
+        mid = (left+right)//2+1
+        last = mid
+        if(B[mid]<a):
+            left = mid+1
+            last = left
+        elif(B[mid]>a):
+            right = mid-1
+            last =right
+        else:
+            return mid
+    return last
+
+
 N = int(input())
 
 A = list(map(int, input().split()))
@@ -12,10 +29,10 @@ B.sort()
 C.sort()
 B2 = []
 for a in A:
-    pos = bisect_right(B, a)
-    B2+=B[pos:]
+    pos = bonary_search(B,a)
+    B2+=B[pos+1:]
 ans = 0
 for b in B2:
-    pos = bisect_right(C, b)
-    ans += N-pos
+    pos = bonary_search(C, b)
+    ans += N-(pos+1)
 print(ans)
